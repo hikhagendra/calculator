c21c6526de18310fc05a2208d314b9fb24e48342
Khagendra Lama <khagendralama2@gmail.com>
2023-07-08 19:04:43 +0545

    Changes made till now

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 let screen = document.getElementById('screen');
 let keys = document.getElementById('keys');
+let userInput = [];
 
 keys.addEventListener('click', function(e) {
-    if(e.target.classList[0] === 'number') {
+    if(e.target.classList[0] === 'number' || e.target.classList[0] === 'operator') {
         addToScreen(e.target.getAttribute('data-value'));
-    } else if (e.target.classList[0] === 'operator') {
-        let currentVal = screen.textContent;
-        
-        if(currentVal[currentVal.length - 1] === ('+' || '-' || '/' || '*')) {
-            // Double operator clicked
-        } else if (currentVal === '') {
-            // Directly operator clicked
-        } else {
-            addToScreen(e.target.getAttribute('data-value'));
-        }
+    } else if(e.target.classList[0] === 'clear') {
+        clearScreen();
     }
 });
 
 function addToScreen(value) {
-    screen.textContent += value;
+    let isPvNum = !isNaN(Number(value));
+    let isSvNum = !isNaN(Number(screen.textContent[screen.textContent.length - 1]));
+    
+    if((isSvNum === false && isPvNum) || (isSvNum && isPvNum) || (isSvNum && isPvNum === false)) {
+        userInput.push(value);
+        screen.textContent = userInput.join('');
+    } else if (isSvNum === false && isPvNum === false) {
+        userInput.pop();
+        userInput.push(value);
+        screen.textContent = userInput.join('');
+    }
 }
+
+function clearScreen() {
+    userInput = [];
+    screen.textContent = '';
+}
+
