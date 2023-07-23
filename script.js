fedac088536302918ed70b0c1353b665c8c4446c
Khagendra Lama <khagendralama2@gmail.com>
2023-07-23 06:27:33 +0545

    Version 1 Calcultor Ready

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ function addToScreen(value) {
     Number(screen.textContent[screen.textContent.length - 1])
   );
 
+  if(screen.textContent === '' && isPvNum === false) {
+    return;
+  }
+
   if (
     (isSvNum === false && isPvNum) ||
     (isSvNum && isPvNum) ||
@@ -42,25 +46,67 @@ function clearScreen() {
 
 function calculate() {
   let expression = userInput.join("");
-  let numbers = expression.match(/\d+/g);
-  let operators = expression.match(/[\+\-\*\/]/g);
-  let output = parseInt(numbers[0]);
-  
-  for(let i = 0; i < operators.length; i++) {
-    let operator = operators[i];
-    let number = parseInt(numbers[i + 1]);
+  let multiplication = expression.match(/(\d+[\*]\d+)+/g);
 
-    if(operator === '+') {
-        output += number;
-    } else if (operator === '-') {
-        output -= number;
-    } else if (operator === '*') {
-        output *= number;
-    } else if (operator === '/') {
-        output /= number;
+  if (multiplication !== null) {
+    for (let i = 0; i < multiplication.length; i++) {
+      expression = expression.replace(
+        multiplication[i],
+        process(multiplication[i])
+      );
     }
   }
 
-  screen.textContent = output;
-  userInput = [output];
+  let division = expression.match(/(\d+[/]\d+)+/g);
+
+  if (division !== null) {
+    for (let i = 0; i < division.length; i++) {
+      expression = expression.replace(
+        division[i],
+        process(division[i])
+      );
+    }
+  }
+
+  let addition = expression.match(/(\d+[\+]\d+)+/g);
+
+  if (addition !== null) {
+    for (let i = 0; i < addition.length; i++) {
+      expression = expression.replace(addition[i], process(addition[i]));
+    }
+  }
+
+  let subtraction = expression.match(/(\d+[\-]\d+)+/g);
+
+  if (subtraction !== null) {
+    for (let i = 0; i < subtraction.length; i++) {
+      expression = expression.replace(subtraction[i], process(subtraction[i]));
+    }
+  }
+
+  screen.textContent = expression;
+  userInput = [expression];
+}
+
+function process(exp) {
+  let numbers = exp.match(/\d+/g);
+  let operators = exp.match(/[\+\-\*\/]/g);
+  let output = parseInt(numbers[0]);
+
+  for (let i = 0; i < operators.length; i++) {
+    let operator = operators[i];
+    let number = parseInt(numbers[i + 1]);
+
+    if (operator === "+") {
+      output += number;
+    } else if (operator === "-") {
+      output -= number;
+    } else if (operator === "*") {
+      output *= number;
+    } else if (operator === "/") {
+      output /= number;
+    }
+  }
+
+  return output;
 }
